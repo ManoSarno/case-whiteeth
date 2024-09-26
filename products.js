d4bfd6ba333fd29ba49719859dac1313f466047a
ManoSarno <manusarno97@gmail.com>
2024-09-26 09:57:32 -0300

    Adding filter script and adjusting products script

--- products.js
+++ products.js
@@ -1,43 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
   const products = [
-    { name: 'Hortelã', category: 'Pasta de dente', categorySlug: 'pasta-de-dente', image: 'img-toothpaste.webp', imageAlt: 'Pasta de dente de Hortelã', rating: 4.5, ratingNumbers: 24, price: 70.90 },
-    { name: 'Morango', category: 'Pasta de dente', categorySlug: 'pasta-de-dente', image: 'img-toothpaste.webp', imageAlt: 'Pasta de dente de Morango', rating: 4.5, ratingNumbers: 24, price: 50.90, comparePrice: 70.90 },
-    { name: 'Uva', category: 'Pasta de dente', categorySlug: 'pasta-de-dente', image: 'img-toothpaste.webp', imageAlt: 'Pasta de dente de Uva', rating: 4.5, ratingNumbers: 24, price: 50.90, comparePrice: 70.90 },
-    { name: 'Café', category: 'Pasta de dente', categorySlug: 'pasta-de-dente', image: 'img-toothpaste.webp', imageAlt: 'Pasta de dente de Café', rating: 4.5, ratingNumbers: 24, price: 50.90, comparePrice: 70.90 },
-    { name: 'Bamboo', category: 'Escova de dentes', categorySlug: 'escova-de-dentes', image: 'img-toothbrush.webp', imageAlt: 'Escova de dentes de Bamboo', rating: 4.5, ratingNumbers: 24, price: 50.90, comparePrice: 70.90 },
-    { name: 'Mógno', category: 'Escova de dentes', categorySlug: 'escova-de-dentes', image: 'img-toothbrush.webp', imageAlt: 'Escova de dentes de Mógno', rating: 4.5, ratingNumbers: 24, price: 50.90, comparePrice: 70.90 },
-    { name: 'Carvalho', category: 'Escova de dentes', categorySlug: 'escova-de-dentes', image: 'img-toothbrush.webp', imageAlt: 'Escova de dentes de Carvalho', rating: 4.5, ratingNumbers: 24, price: 50.90, comparePrice: 70.90 },
-    { name: 'Fio dental', category: 'Outros', categorySlug: 'outros', image: 'img-others.webp', imageAlt: 'Fio dental', rating: 4.5, ratingNumbers: 24, price: 50.90, comparePrice: 70.90 },
-    { name: 'Fio dental', category: 'Outros', categorySlug: 'outros', image: 'img-others.webp', imageAlt: 'Fio dental', rating: 4.5, ratingNumbers: 24, price: 50.90, comparePrice: 70.90 }
+    { name: 'Hortelã', category: 'Pasta de dente', image: 'img-toothpaste.webp', imageAlt: 'Pasta de dente de Hortelã', rating: 4.5, ratingNumbers: 24, price: 70.90 },
+    { name: 'Morango', category: 'Pasta de dente', image: 'img-toothpaste.webp', imageAlt: 'Pasta de dente de Morango', rating: 4.5, ratingNumbers: 24, price: 50.90, comparePrice: 70.90 },
+    { name: 'Uva', category: 'Pasta de dente', image: 'img-toothpaste.webp', imageAlt: 'Pasta de dente de Uva', rating: 4.5, ratingNumbers: 24, price: 50.90, comparePrice: 70.90 },
+    { name: 'Café', category: 'Pasta de dente', image: 'img-toothpaste.webp', imageAlt: 'Pasta de dente de Café', rating: 4.5, ratingNumbers: 24, price: 50.90, comparePrice: 70.90 },
+    { name: 'Bamboo', category: 'Escova de dente', image: 'img-toothbrush.webp', imageAlt: 'Escova de dentes de Bamboo', rating: 4.5, ratingNumbers: 24, price: 50.90, comparePrice: 70.90 },
+    { name: 'Mógno', category: 'Escova de dente', image: 'img-toothbrush.webp', imageAlt: 'Escova de dentes de Mógno', rating: 4.5, ratingNumbers: 24, price: 50.90, comparePrice: 70.90 },
+    { name: 'Carvalho', category: 'Escova de dente', image: 'img-toothbrush.webp', imageAlt: 'Escova de dentes de Carvalho', rating: 4.5, ratingNumbers: 24, price: 50.90, comparePrice: 70.90 },
+    { name: 'Fio dental', category: 'Outros', image: 'img-others.webp', imageAlt: 'Fio dental', rating: 4.5, ratingNumbers: 24, price: 50.90, comparePrice: 70.90 },
+    { name: 'Fio dental', category: 'Outros', image: 'img-others.webp', imageAlt: 'Fio dental', rating: 4.5, ratingNumbers: 24, price: 50.90, comparePrice: 70.90 }
   ];
 
-  function getCategoriesWithSlugs(products) {
-    // Extrai categorias e remove duplicatas
-    const categories = [...new Set(products.map(product => product.category))];
-
-    // Gera um array de objetos com nome da categoria e slug
-    return categories.map(category => ({
-        name: category,
-        slug: category.toLowerCase().replace(/\s+/g, '-')
-    }));
-}
-
-// Chama a função para obter as categorias com slugs
-const categoriesWithSlugs = getCategoriesWithSlugs(products);
+  // Array com todas as categorias
+  const categories = [...new Set(products.map(product => product.category))];
 
   const productsContainer = document.getElementById('products');
 
   // Loop por todas as categorias
-  categoriesWithSlugs.forEach((category, index) => {
+  categories.forEach((category) => {
     let categoryWrapper = document.createElement('article');
-    categoryWrapper.id = category.slug;
+    
     categoryWrapper.classList.add('product-category');
     let categoryHeadingWrapper = document.createElement('div');
     categoryHeadingWrapper.classList.add('container');
 
     let categoryHeading = document.createElement('h3');
     categoryHeading.classList.add('category-heading');
-    categoryHeading.textContent = category.name;
+    categoryHeading.textContent = category;
 
     categoryHeadingWrapper.append(categoryHeading);
 
@@ -50,7 +39,7 @@ const categoriesWithSlugs = getCategoriesWithSlugs(products);
     categoryProductsWrapper.append(categoryProducts);
 
     // Filtra os produtos que pertencem à categoria atual
-    const productsInCategory = products.filter(product => product.category === category.name);
+    const productsInCategory = products.filter(product => product.category === category);
     console.log(productsInCategory);
 
     // Loop por todos os produtos da categoria
